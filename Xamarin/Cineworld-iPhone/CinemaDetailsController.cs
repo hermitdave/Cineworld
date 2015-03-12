7586972f53d113efa01785e0ebded5ded5c69ad4
hermitd@hotmail.com <hd34lon!>
2015-03-12 10:21:08 +0000

    Added Cinema Gist to Cinema Details view

--- Xamarin/Cineworld-iPhone/CinemaDetailsController.cs
+++ Xamarin/Cineworld-iPhone/CinemaDetailsController.cs
@@ -2,6 +2,8 @@ using Foundation;
 using System;
 using System.CodeDom.Compiler;
 using UIKit;
+using PDRatingSample;
+using System.Drawing;
 
 namespace CineworldiPhone
 {
@@ -12,5 +14,32 @@ namespace CineworldiPhone
 		}
 
 		public CinemaInfo Cinema { get; set; }
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			this.CinemaTitle.Title = this.Cinema.Name;
+
+			// Gather up the images to be used.
+			RatingConfig ratingConfig = new RatingConfig(UIImage.FromFile("Images/Stars/empty.png"), UIImage.FromFile("Images/Stars/filled.png"), UIImage.FromFile("Images/Stars/chosen.png"));
+
+			// Create the view.
+			var ratingView = new PDRatingView(new RectangleF(20f, 0f, 50f, 30f), ratingConfig, Convert.ToDecimal(this.Cinema.AverageRating));
+
+			// [Required] Add the view to the 
+			this.CinemaGist.AddSubview(ratingView);
+
+			var reviewCount = new UILabel (new RectangleF (80f, 0f, 60f, 30f));
+			reviewCount.Font = UIFont.FromName ("HelveticaNeue", 12f);
+			reviewCount.Text = String.Format ("{0} ratings", this.Cinema.Reviews.Count);
+			this.CinemaGist.AddSubview (reviewCount);
+
+			this.Address.Text = this.Cinema.FullAddress;
+
+			this.Telephone.Text = this.Cinema.Telephone;
+
+			this.ReviewsTable.Source = new ReviewsTableSource (this.Cinema.Reviews);
+		}
 	}
 }
--- Xamarin/Cineworld-iPhone/CinemaDetailsController.designer.cs
+++ Xamarin/Cineworld-iPhone/CinemaDetailsController.designer.cs
@@ -14,8 +14,64 @@ namespace CineworldiPhone
 	[Register ("CinemaDetailsController")]
 	partial class CinemaDetailsController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel Address { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIView CinemaGist { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UISegmentedControl CinemaSegments { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UINavigationItem CinemaTitle { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton RateReview { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITableView ReviewsTable { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel Telephone { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (Address != null) {
+				Address.Dispose ();
+				Address = null;
+			}
+			if (CinemaGist != null) {
+				CinemaGist.Dispose ();
+				CinemaGist = null;
+			}
+			if (CinemaSegments != null) {
+				CinemaSegments.Dispose ();
+				CinemaSegments = null;
+			}
+			if (CinemaTitle != null) {
+				CinemaTitle.Dispose ();
+				CinemaTitle = null;
+			}
+			if (RateReview != null) {
+				RateReview.Dispose ();
+				RateReview = null;
+			}
+			if (ReviewsTable != null) {
+				ReviewsTable.Dispose ();
+				ReviewsTable = null;
+			}
+			if (Telephone != null) {
+				Telephone.Dispose ();
+				Telephone = null;
+			}
 		}
 	}
 }
